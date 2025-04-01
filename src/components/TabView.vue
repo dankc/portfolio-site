961ea925b9f98b091ed129e905d427bebd8b0bb7
dankc <dankc@pm.me>
2025-04-01 15:05:42 -0500

    Adding tabbed component and adding it to Feat. Proj.

---
+++ src/components/TabView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="tab-view">
+    <div class="tab-view__nav-container">
+      <slot name="nav" />
+      <nav class="tab-view__nav">
+        <div
+          class="tab-view__nav-item"
+          :class="{ active: currentTab === index }"
+          v-for="(tab, index) of tabs"
+          :key="index"
+          @click="changeTab(index)"
+        >
+          {{ tab }}
+        </div>
+      </nav>
+    </div>
+    <div class="tab-view__pane">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineProps, ref } from 'vue';
+
+  const emit = defineEmits<{
+    (e: 'tabChange', value: number): void;
+  }>();
+
+  const { tabs } = defineProps<{
+    tabs: string[];
+  }>();
+
+  const currentTab = ref(0);
+
+  const changeTab = (tab: number) => {
+    currentTab.value = tab;
+    emit('tabChange', currentTab.value);
+  };
+</script>
+
+<style>
+  .tab-view__nav-item {
+    cursor: pointer;
+  }
+</style>
