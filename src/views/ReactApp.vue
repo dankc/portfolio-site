a037e1a651351e50dec42c4749f2a0a7a6e3530f
dankc <dankc@pm.me>
2025-12-04 23:21:52 -0600

    Attempting to fix build error

--- src/views/ReactApp.vue
+++ src/views/ReactApp.vue
@@ -9,6 +9,7 @@
   let unmount: () => void;
 
   onMounted(async () => {
+    // @ts-expect-error this comes from federation and won't be available during a build
     const ReactComponent = await import('react_app/App');
 
     ReactComponent.mount(reactContainer.value);
--- vite.config.ts
+++ vite.config.ts
@@ -84,6 +84,7 @@ export default defineConfig({
     },
   },
   build: {
+    target: 'esnext',
     minify: !isDev,
     cssMinify: !isDev,
   },
